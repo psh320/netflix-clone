3c7f9f0c2ed6dc736a0c1a66e89beae4bae6f597
Andrew Park <parksungho320@gmail.com>
2022-04-11 02:23:14 +0900

    Added src folder

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import RootReducer from './store/reducers';
+import { composeWithDevTools } from 'redux-devtools-extension'; // 리덕스 개발자 도구
+
+const persistedState = localStorage.getItem("reduxState") 
+  ? JSON.parse(localStorage.getItem('reduxState'))
+  : {}
+
+const store = createStore(RootReducer, persistedState, composeWithDevTools());
+
+store.subscribe(()=> {
+  localStorage.setItem('reduxState', JSON.stringify(store.getState()))
+})
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
